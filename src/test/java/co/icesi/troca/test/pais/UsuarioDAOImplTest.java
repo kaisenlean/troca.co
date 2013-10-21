cec81296e8140777eeceebee327ed0f43795e132
Elmer Jose Diaz Lazo <elmerdiazlazo@gmail.com>
2013-10-21 14:22:07 -0500

    solucion definitiva  al modulo de testing

--- src/test/java/co/icesi/troca/test/pais/UsuarioDAOImplTest.java
+++ src/test/java/co/icesi/troca/test/pais/UsuarioDAOImplTest.java
@@ -6,13 +6,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
 import co.icesi.troca.repositories.UsuarioDAO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"file:WebContent/WEB-INF/applicationContext.xml"})
-public class UsuarioDAOImplTest {
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class})
+public class UsuarioDAOImplTest extends AbstractTransactionalJUnit4SpringContextTests {
 
 	@Autowired
 	private UsuarioDAO dao;
@@ -21,6 +26,13 @@ public class UsuarioDAOImplTest {
 	@Test
 	public void testCreate() {
 //		fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	public void testFindAll(){
+		System.out.println(dao.findAll());
+		
 	}
 	public void setDao(UsuarioDAO dao) {
 		this.dao = dao;
