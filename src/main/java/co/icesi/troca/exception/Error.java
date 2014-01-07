ff4988b1cf06f8c59d07917780e984b121e63bea
Elmer Jose Diaz Lazo <elmerdiazlazo@gmail.com>
2014-01-07 13:14:21 -0500

    cambio en guias

--- src/main/java/co/icesi/troca/exception/Error.java
+++ src/main/java/co/icesi/troca/exception/Error.java
@@ -24,6 +24,7 @@ public class Error implements Serializable {
 	public Error() {
 	}
 
+	@SuppressWarnings("rawtypes")
 	public String getStackTrace() {
 		// Get the current JSF context
 		FacesContext context = FacesContext.getCurrentInstance();
